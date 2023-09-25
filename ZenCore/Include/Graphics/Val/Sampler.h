4587ce29df308800e762ced18e4b44819f0146c1
proton991 <99615819@qq.com>
2023-09-25 10:09:38 +0800

    add Sampler wrapper

---
+++ ZenCore/Include/Graphics/Val/Sampler.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "DeviceObject.h"
+
+namespace zen::val
+{
+class Sampler : public DeviceObject<VkSampler, VK_OBJECT_TYPE_SAMPLER>
+{
+public:
+    explicit Sampler(Device& device, VkFilter filter = VK_FILTER_NEAREST, VkSamplerAddressMode addressMode = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE);
+
+    Sampler(Sampler&& other) noexcept;
+
+    ~Sampler();
+};
+} // namespace zen::val
---
+++ ZenCore/Source/Graphics/Val/Sampler.cpp
@@ -0,0 +1,36 @@
+#include "Graphics/Val/Sampler.h"
+#include "Common/Errors.h"
+
+namespace zen::val
+{
+Sampler::Sampler(Device& device, VkFilter filter, VkSamplerAddressMode addressMode) :
+    DeviceObject(device)
+{
+    VkSamplerCreateInfo samplerCI{};
+    samplerCI.magFilter     = filter;
+    samplerCI.minFilter     = filter;
+    samplerCI.addressModeU  = addressMode;
+    samplerCI.addressModeV  = addressMode;
+    samplerCI.addressModeW  = addressMode;
+    samplerCI.mipLodBias    = 0.0f;
+    samplerCI.maxAnisotropy = 1.0f;
+    samplerCI.minLod        = 0.0f;
+    samplerCI.maxLod        = 1.0f;
+    samplerCI.borderColor   = VK_BORDER_COLOR_FLOAT_OPAQUE_WHITE;
+
+    CHECK_VK_ERROR_AND_THROW(vkCreateSampler(m_device.GetHandle(), &samplerCI, nullptr, &m_handle), "Failed to create sampler");
+}
+
+Sampler::Sampler(Sampler&& other) noexcept :
+    DeviceObject(std::move(other))
+{
+}
+
+Sampler::~Sampler()
+{
+    if (m_handle != VK_NULL_HANDLE)
+    {
+        vkDestroySampler(m_device.GetHandle(), m_handle, nullptr);
+    }
+}
+} // namespace zen::val
